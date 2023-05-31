ea3400e843069f154a0e161699673acb3e282f99
alexiussev1 <alexius.sev@gmail.com>
2023-06-01 00:38:22 +0300

    Приступил к ДЗ1

--- src/main/java/seminar1/Main.java
+++ src/main/java/seminar1/Main.java
@@ -21,10 +21,11 @@ package seminar1;
 public class Main {
     public static void main(String[] args) {
 
-        Vending vending = new Vending();
-        Product product1 = new Product("Apple", 20, 1L);
-        Product product2 = new Product("Orange", 30, 2L);
-        Product product3 = new Product("Banana", 25, 3L);
+        Vending vending = new FruitVending();
+
+        Product product1 = new Apple("Apple", 20, 1L);
+        Product product2 = new Orange("Orange", 30, 2L);
+        Product product3 = new Banana("Banana", 25, 3L);
 
         vending.addProduct(product1);
         vending.addProduct(product2);
@@ -32,9 +33,13 @@ public class Main {
 
         //product1.kind = "Apple";
 
-        vending.addProduct(product1);
         System.out.println(vending.getProduct("Apple"));
+        System.out.println(vending.getProduct("Orange"));
+        System.out.println(vending.getProduct("Banana"));
 
+        System.out.println(vending.getProduct(1L));
+        System.out.println(vending.getProduct(2L));
+        System.out.println(vending.getProduct(5L));
 
     }
 }
--- src/main/java/seminar1/Vending.java
+++ src/main/java/seminar1/Vending.java
@@ -6,16 +6,12 @@ import java.util.List;
 //... и ТорговыйАвтомат,
 //  содержащий в себе методы initProducts (List <Product>)
 //  сохраняющий в себе список исходных продуктов и getProduct(String name)
-public class Vending {
+public abstract class Vending {
     private final List<Product> list; // = new ArrayList<>();
 
     public Vending() {
         list = new ArrayList<>();
     }
-
-
-
-
 
 
     void addProduct(Product product) {
@@ -29,6 +25,16 @@ public class Vending {
             }
         }
         return null;
+    }
+
+    Product getProduct(Long id) {
+        for (Product product : list) {
+            if (id.equals(product.getId())){
+                return product;
+            }
+            
+        }
+        return  null;
     }
 
 }
